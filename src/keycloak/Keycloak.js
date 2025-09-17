8803d561a893f48698e9afa89ff9e396f2ddcdde
RAJA PINJA <raja_pinja@yahoo.com>
2025-09-17 23:23:42 +0530

    Remove duplicate files from netwatch

--- src/keycloak/Keycloak.js
+++
@@ -1,9 +0,0 @@
-import Keycloak from "keycloak-js";
-
-const keycloak = new Keycloak({
-  url: "http://localhost:8383",  // 👈 add /auth
-  realm: "netwatch-realm",
-  clientId: "netwatch-ui",
-});
-
-export default keycloak;
